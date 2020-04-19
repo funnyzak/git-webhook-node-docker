0638374287733ab8878735f91a5c18f23959e753
Leon <silenceace@gmail.com>
2020-04-19 20:59:35 +0800

    fixed

--- scripts/run_scripts_on_startup.sh
+++ scripts/run_scripts_on_startup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-for file in /custom_scripts/on_startup/*; do
-    "$file"
-done
+
+if [ -n "$(ls -A /custom_scripts/on_startup/* 2>/dev/null)" ]; then
+    for file in /custom_scripts/on_startup/*; do
+        "$file"
+    done
+else 
+    echo "no files. skip."
+fi
